1755ce118d17e000f999b4a48f76f383ae2c0600
chinmayajeet <chinmayajeet@IBM887-PC0DXRML.in.ibm.com>
2018-07-20 14:59:42 +0530

    Test Commit

---
+++ src/main/java/com/ibm/callforcode/poc/sampleservice/SampleMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.callforcode.poc.sampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMicroserviceApplication.class, args);
+	}
+}
